c61d3237dd25522cc60f0db760f465697cde1938
1585832651 <1585832651@qq.com>
2024-02-25 11:30:51 +0800

    restormer

---
+++ train_restormer.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=4329 basicsr/train.py -opt ./options/train/CVHSSR_Tx4_restormer.yml --launcher pytorch
+# CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=1 --master_port=4329 basicsr/train.py -opt ./options/train/CVHSSR_Tx4.yml --launcher pytorch
